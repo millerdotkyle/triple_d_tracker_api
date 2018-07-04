37f1611bf410e9b187cd75fc153fb6466b5fa156
Peter King <73837king@gmail.com>
2018-07-04 13:27:18 -0400

    basic get all locations route coded,tested in postman, works with requireAuth

--- controllers/locationsController.js
+++ controllers/locationsController.js
@@ -1,19 +1,31 @@
 'use strict';
 const config = require('../config');
 const mongoose = require('mongoose');
+const {Location} = require('../models/model_locations');
 
 mongoose.connect(config.DATABASE_URL);
 
-exports.getAllLocations = (req, res) => {
-  console.log('locationController getAllLocations req.body = ', req.body);
-};
+module.exports = {
+  // NOTE: alternate module.export syntax than authController.js exports.signup syntax.
+  getAllLocations(req, res, next) {
+    console.log('locationController getAllLocations req.body = ', req.body);
+    Location
+      .find()
+      .then((locations) => { res.json(locations).status(200); })
+      .catch(err => {
+        console.error(err);
+        res.status(500).json({ message: 'Internal server error', err: err });
+      });
 
-exports.getLocationsByState = (req, res) => {
-  const state = req.params;
-  console.log('locationController getLocationsByState req.body = ', req.body);
-  console.log('locationController getLocationsByState req.params = ', req.params);
-};
+  },
 
-exports.getLocationsNearMe = (req, res) => {
-  console.log('locationController getLocationsNearMe req.body = ', req.body);
+  getLocationsByState(req, res, next) {
+    const state = req.params;
+    console.log('locationController getLocationsByState req.body = ', req.body);
+    console.log('locationController getLocationsByState req.params = ', req.params);
+  },
+
+  getLocationsNearme(req, res, next) {
+    console.log('locationController getLocationsNearMe req.body = ', req.body);
+  }
 };
